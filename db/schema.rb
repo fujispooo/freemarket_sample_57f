ee5cee2ee7e802f5daf53fe18708d3074e5de00b
fujipooo <majesty1579@yahoo.co.jp>
2019-08-28 09:31:17 +0900

    usersテーブルにbirth_yaer,birth_month,birth_dayカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_27_103504) do
+ActiveRecord::Schema.define(version: 2019_08_28_002850) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,8 +27,10 @@ ActiveRecord::Schema.define(version: 2019_08_27_103504) do
     t.string "last_name_kana", null: false
     t.text "introduction"
     t.string "avatar"
-    t.date "birthday"
     t.integer "phone_number", null: false
+    t.integer "birth_yaer"
+    t.integer "birth_month"
+    t.integer "birth_day"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
