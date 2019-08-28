ef1ad0358f27a945952a1fec1ae86ba21efe3152
Ryousuke-Nishioka <ry0sk6336.oki@gmail.com>
2019-08-28 15:45:30 +0900

    ハッシュロケット修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'card# method'
+  root to: 'card# information'
   resources :items do
     collection do
       get 'mypage/identification' => 'items#show'
