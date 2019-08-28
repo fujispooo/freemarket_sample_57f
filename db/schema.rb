29c2ddf326a0d789efd8bca7ace361bd4fff2e18
fujipooo <majesty1579@yahoo.co.jp>
2019-08-28 15:45:59 +0900

    cardsテーブル、モデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_025238) do
+ActiveRecord::Schema.define(version: 2019_08_28_064003) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 2019_08_28_025238) do
     t.bigint "user_id", null: false
     t.integer "prefecture_id"
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "card_number", null: false
+    t.integer "explation_month", null: false
+    t.integer "explation_year", null: false
+    t.integer "security_code", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
   create_table "prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -54,4 +65,5 @@ ActiveRecord::Schema.define(version: 2019_08_28_025238) do
   end
 
   add_foreign_key "addresses", "users"
+  add_foreign_key "cards", "users"
 end
