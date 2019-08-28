3eee8754e29b6569796d135ddb534201906b2ea8
fujipooo <majesty1579@yahoo.co.jp>
2019-08-29 02:30:39 +0900

    routing変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,35 @@
 Rails.application.routes.draw do
   root to: 'profile#index'
-  # # devise_for :users
-  # root to: 'items#index'
+  # devise_for :users
+  root to: 'items#index'
   
 
 
-  # resources :users do
-  # end
+  resources :users do
+  end
 
 
 
-  # resources :items do
-  #   collection do
-  #     get 'mypage/identification' => 'items#show'
-  #     get 'mypage/logout'         => 'items#logout'
-  #     get :test1
-  #     get :test2
-  #     get :test3
-  #     get :test4
-  #     get :test5
-  #     get :test6
-  #     get :test7
-  #     get :test8
-  #     get :test9
-  #   end
+  resources :items do
+    collection do
+      get 'mypage/identification' => 'items#show'
+      get 'mypage/logout'         => 'items#logout'
+      get :test1
+      get :test2
+      get :test3
+      get :test4
+      get :test5
+      get :test6
+      get :test7
+      get :test8
+      get :test9
+    end
 
-  # end 
+  end 
 
-  # # devise_for :users
-  # root 'items#index'
-  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # devise_for :users
+  root 'items#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   end
 
