0192460a7ea15e8b6df4037214e9ec97c47f0a06
fujipooo <majesty1579@yahoo.co.jp>
2019-08-29 16:35:52 +0900

    ザオラルmigration

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users, controllers: {
+  devise_for :users, controllers:{
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   } 
