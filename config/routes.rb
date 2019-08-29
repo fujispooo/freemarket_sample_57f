10c275f485486d0c1f0e7ee1da8621d4b091dbb3
fujipooo <majesty1579@yahoo.co.jp>
2019-08-29 13:19:55 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
 
-  root to: 'items#index'
-end
-
-  root to: 'profile#index'
-  root to: 'items#index'
   devise_for :users
   root to: 'items#index'
+  root to: 'profile#index'
   root to: 'details#index'
 
   resources :users do
@@ -28,30 +24,4 @@ end
       get 'mypage/logout'         => 'items#logout'
     end
   end
-
-
-  # resources :items do
-  #   collection do
-  #     get 'mypage/identification' => 'items#show'
-  #     get 'mypage/logout'         => 'items#logout'
-  #     get :test1
-  #     get :test2
-  #     get :test3
-  #     get :test4
-  #     get :test5
-  #     get :test6
-  #     get :test7
-  #     get :test8
-  #     get :test9
-  #   end
-
-  # end 
-
-  # # devise_for :users
-  # root 'items#index'
-  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  end
-
-
 end
