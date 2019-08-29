335dc37efd116c2b48e19f7ed00b65bf8a41b9d5
fujipooo <majesty1579@yahoo.co.jp>
2019-08-29 23:03:15 +0900

    @mixinにフォームのスタイルを定義

--- db/migrate/20190828002850_add_birth_year_to_user.rb
+++ db/migrate/20190828002850_add_birth_year_to_user.rb
@@ -1,6 +1,6 @@
 class AddBirthYearToUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :birth_yaer, :integer
+    add_column :users, :birth_year, :integer
     add_column :users, :birth_month, :integer
     add_column :users, :birth_day, :integer
   end
