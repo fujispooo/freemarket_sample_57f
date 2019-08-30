36a3034cc5b163a6763e78f6e70f52a60b2873d4
kcazuki <53326300+kcazuki@users.noreply.github.com>
2019-08-30 12:16:15 +0900

    ヘッダー分けた

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'items#index'
+  root to: 'items#top'
   root to: 'details#index'
   
   resources :users do
@@ -32,6 +32,7 @@ Rails.application.routes.draw do
       get 'mypage/logout' => 'items#logout'
       get :purchase
       get 'items/show/' => 'items#show'
+      get 'itemes/top/' => 'items#top'
     end
   end
 end
