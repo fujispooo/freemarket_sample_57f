3358346341c916e02f44ea1c629d738ff1561651
domesoo <takya.domesoo@gmail.com>
2019-08-31 12:37:19 +0900

    delivery_method model and table creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_31_032931) do
+ActiveRecord::Schema.define(version: 2019_08_31_033409) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -56,6 +56,12 @@ ActiveRecord::Schema.define(version: 2019_08_31_032931) do
 
   create_table "delivery_fees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "delivery_fee", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "delivery_methods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "delivery_method", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
