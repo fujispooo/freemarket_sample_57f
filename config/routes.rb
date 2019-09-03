50d48b118ae97b499888043b2b7de991983dd91d
kcazuki <53326300+kcazuki@users.noreply.github.com>
2019-09-03 14:39:43 +0900

    商品詳細ページのパス設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'items#top'
+  root to: 'items#index'
   devise_for :users, controllers:{
     registrations: 'users/registrations',
     sessions: 'users/sessions'
