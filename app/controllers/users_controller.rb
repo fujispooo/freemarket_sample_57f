0ff61d9270cb6d528c8cb9da9874e37a32e31878
Ryousuke-Nishioka <ry0sk6336.oki@gmail.com>
2019-09-11 15:19:23 +0900

    本人情報編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  def identification
+    # @item = Item.find(params[:item_id])
+    @address = Address.find(current_user.id)
+    @user = User.find(current_user.id)
+  end
+
   def show
   end
 
