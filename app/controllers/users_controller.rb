f8af19f37878d78a075feb4f6ca1989a7aa78f21
Ryousuke-Nishioka <ry0sk6336.oki@gmail.com>
2019-09-11 17:16:17 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def identification
-    # @item = Item.find(params[:item_id])
     @address = Address.find(current_user.id)
     @user = User.find(current_user.id)
   end
