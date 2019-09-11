c57c8022648d9faf7d03d7b829ed4b9483ed6e3c
Ryousuke-Nishioka <ry0sk6336.oki@gmail.com>
2019-09-11 12:48:01 +0900

    商品確認画面からのカード決済

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   has_one :address
-  has_one :card
   accepts_nested_attributes_for :address
+  has_one :card, dependent: :destroy
   accepts_nested_attributes_for :card
 
   #  googleとfacebookのsns認証
