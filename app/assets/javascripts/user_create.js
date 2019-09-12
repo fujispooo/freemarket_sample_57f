0a92d4821030a0e455e1ce43aeb6033e2f5e2f6a
fujipooo <majesty1579@yahoo.co.jp>
2019-09-12 21:05:00 +0900

    nickname,email等バリデーション完成

--- app/assets/javascripts/user_create.js
+++ app/assets/javascripts/user_create.js
@@ -1,140 +1,127 @@
-$(function(){
-  $("#user_signup-form").validate({
-    rules : {
-      "user[nickname]":{
-        required: true,
-        minlength: 6
+window.addEventListener("turbolinks:load", function() {
+  // 全角カタカナのみ
+  jQuery.validator.addMethod("katakana", function(value, element) {
+    return this.optional(element) || /^([ァ-ヶー]+)$/.test(value);
+    }, "<br/>全角カタカナを入力してください"
+  );
+  $(function(){
+    $("#user_signup-form").validate({
+      rules : {
+        "user[nickname]":{
+          required: true,
+          minlength: 2,
+          maxlength: 10
+        },
+        "user[email]":{
+          required: true,
+          email: true
+        },
+        "user[password]":{
+          required: true,
+          password: true
+        },
+        "user[password_confirmation]":{
+          required: true,
+          password: true,
+          alphanum: true,
+          equalTo: "#user_password"
+        },
+        "user[first_name]":{
+          required: true
+        },
+        "user[last_name]":{
+          required: true
+        },
+        "user[first_name_kana]":{
+          required: true,
+          katakana: true
+        },
+        "user[last_name_kana]":{
+          required: true,
+          katakana: true
+        },
+        "user[birth_year]":{
+          required: true
+        },
+        "user[birth_month]":{
+          required: true
+        },
+        "user[birth_day]":{
+          required: true
+        }
       },
-      "user[email]":{
-        required: true,
-        email: true
+      messages : {
+        "user[nickname]":{
+          required: "ニックネーム は必須項目です",
+          minlength: "2文字以上で入力してください",
+          maxlength: "10文字以内で入力してください"
+        },
+        "user[email]":{
+          required: "メールアドレス は必須項目です",
+        },
+        "user[password]":{
+          required: "パスワード は必須項目です",
+        },
+        "user[password_confirmation]":{
+          required: "パスワード再認証 は必須項目です",
+        },
+        "user[first_name]":{
+          required: "姓 は必須項目です"
+        },
+        "user[last_name]":{
+          required: "名 は必須項目です"
+        },
+        "user[first_name_kana]":{
+          required: "姓(カナ) は必須項目です"
+        },
+        "user[last_name_kana]":{
+          required: "名(カナ) は必須項目です"
+        },
+        "user[birth_year]":{
+          required: "\"年\" は必須項目です"
+        },
+        "user[birth_month]":{
+          required: "\"月\" は必須項目です"
+        },
+        "user[birth_day]":{
+          required: "\"日\" は必須項目です"
+        }
       },
-      "user[password]":{
-        required: true,
-        password: true
-      },
-      "user[password_confirmation]":{
-        required: true,
-        password: true,
-        equalTo: "#user[password]"
-      },
-      "user[first_name]":{
-        required: true
-      },
-      "user[last_name]":{
-        required: true
-      },
-      "user[first_name_kana]":{
-        required: true
-      },
-      "user[last_name_kana]":{
-        required: true
-      },
-      "user[birth_year]":{
-        required: true
-      },
-      "user[birth_month]":{
-        required: true
-      },
-      "user[birth_day]":{
-        required: true
+      errorPlacement: function(error, element) {
+        if (element.attr('name') == 'user[nickname]') {
+          error.appendTo($('#emsg_nickname'));
+        }
+        if (element.attr('name') == 'user[email]') {
+          error.appendTo($('#emsg_email'));
+        }
+        if (element.attr('name') == 'user[password]') {
+          error.appendTo($('#emsg_password'));
+        }
+        if (element.attr('name') == 'user[password_confirmation]') {
+          error.appendTo($('#emsg_password_confirmation'));
+        }
+        if (element.attr('name') == 'user[first_name]') {
+          error.appendTo($('#emsg_first_name'));
+        }
+        if (element.attr('name') == 'user[last_name]') {
+          error.appendTo($('#emsg_last_name'));
+        }
+        if (element.attr('name') == 'user[first_name_kana]') {
+          error.appendTo($('#emsg_first_name_kana'));
+        }
+        if (element.attr('name') == 'user[last_name_kana]') {
+          error.appendTo($('#emsg_last_name_kana'));
+        }
+        if (element.attr('name') == 'user[birth_year]') {
+          error.appendTo($('#emsg_birth_year'));
+        }
+        if (element.attr('name') == 'user[birth_month]') {
+          error.appendTo($('#emsg_birth_month'));
+        }
+        if (element.attr('name') == 'user[birth_day]') {
+          error.appendTo($('#emsg_birth_day'));
+        }
       }
-    },
-    messages : {
-      "user[nickname]":{
-        required: "必須項目です"
-      },
-      "user[email]":{
-        required: "必須項目です",
-      },
-      "user[password]":{
-        required: "必須項目です",
-      },
-      "user[password_confirmation]":{
-        required: "必須項目です",
-      },
-      "user[first_name]":{
-        required: "必須項目です"
-      },
-      "user[last_name]":{
-        required: "必須項目です"
-      },
-      "user[first_name_kana]":{
-        rrequired: "必須項目です"
-      },
-      "user[last_name_kana]":{
-        required: "必須項目です"
-      },
-      "user[birth_year]":{
-        required: "必須項目です"
-      },
-      "user[birth_month]":{
-        required: "必須項目です"
-      },
-      "user[birth_day]":{
-        required: "必須項目です"
-      }
-    },
-    errorPlacement: function(error, element) {
-      if (element.attr('name') == 'user[nickname]') {
-        error.appendTo($('#emsg_nickname'));
-      }
-    },
-    errorPlacement: function(error, element) {
-      if (element.attr('name') == 'user[email]') {
-        error.appendTo($('#emsg_email'));
-      }
-    },
-    errorPlacement: function(error, element) {
-      if (element.attr('name') == 'user[password]') {
-        error.appendTo($('#emsg_password'));
-      }
-    },
-    errorPlacement: function(error, element) {
-      if (element.attr('name') == 'user[password_confirmation]') {
-        error.appendTo($('#emsg_password_confirmation'));
-      }
-    },
-    errorPlacement: function(error, element) {
-      if (element.attr('name') == 'user[nickname]') {
-        error.appendTo($('#emsg_nickname'));
-      }
-    },
-    errorPlacement: function(error, element) {
-      if (element.attr('name') == 'user[nickname]') {
-        error.appendTo($('#emsg_nickname'));
-      }
-    },
-    errorPlacement: function(error, element) {
-      if (element.attr('name') == 'user[nickname]') {
-        error.appendTo($('#emsg_nickname'));
-      }
-    },
-    errorPlacement: function(error, element) {
-      if (element.attr('name') == 'user[nickname]') {
-        error.appendTo($('#emsg_nickname'));
-      }
-    },
-    errorPlacement: function(error, element) {
-      if (element.attr('name') == 'user[nickname]') {
-        error.appendTo($('#emsg_nickname'));
-      }
-    },
-    errorPlacement: function(error, element) {
-      if (element.attr('name') == 'user[nickname]') {
-        error.appendTo($('#emsg_nickname'));
-      }
-    },
-    errorPlacement: function(error, element) {
-      if (element.attr('name') == 'user[nickname]') {
-        error.appendTo($('#emsg_nickname'));
-      }
-    },
-    errorPlacement: function(error, element) {
-      if (element.attr('name') == 'user[nickname]') {
-        error.appendTo($('#emsg_nickname'));
-      }
-    }
+    });
   });
-  debugger
 });
