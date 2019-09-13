cffa1324cf62ac33ceb1dea53b7a1e1cf5816f76
fujipooo <majesty1579@yahoo.co.jp>
2019-09-13 16:12:53 +0900

    change_validation

--- app/assets/javascripts/user_create.js
+++ app/assets/javascripts/user_create.js
@@ -27,12 +27,10 @@ window.addEventListener("turbolinks:load", function() {
       },
       "user[password]":{
         required: true,
-        password: true,
         minlength: 7
       },
       "user[password_confirmation]":{
         required: true,
-        password: true,
         minlength: 7,
         equalTo: "#user_password"
       },
@@ -82,61 +80,64 @@ window.addEventListener("turbolinks:load", function() {
     },
     messages : {
       "user[nickname]":{
-        required: "ニックネーム は必須項目です",
-        minlength: "2文字以上で入力してください",
-        maxlength: "10文字以内で入力してください"
+        required: "※ ニックネーム は必須項目です",
+        minlength: "※ 2文字以上で入力してください",
+        maxlength: "※ 10文字以内で入力してください"
       },
       "user[email]":{
-        required: "メールアドレス は必須項目です",
+        required: "※ メールアドレス は必須項目です",
       },
       "user[password]":{
-        required: "パスワード は必須項目です",
+        required: "※ パスワード は必須項目です",
+        minlength: "※ 7文字以上で入力してください"
       },
       "user[password_confirmation]":{
-        required: "パスワード再認証 は必須項目です",
+        required: "※ パスワード再認証 は必須項目です",
+        minlength: "※ 7文字以上で入力してください",
+        equalTo: "※ passwordの確認が取れませんでした"
       },
       "user[first_name]":{
-        required: "姓 は必須項目です"
+        required: "※ 姓 は必須項目です"
       },
       "user[last_name]":{
-        required: "名 は必須項目です"
+        required: "※ 名 は必須項目です"
       },
       "user[first_name_kana]":{
-        required: "姓(カナ) は必須項目です",
-        katakana:"姓(カナ) はは全角カタカナで入力してください"
+        required: "※ 姓(カナ) は必須項目です",
+        katakana:"※ 姓(カナ) はは全角カタカナで入力してください"
       },
       "user[last_name_kana]":{
-        required: "名(カナ) は必須項目です",
-        katakana:"名(カナ) は全角カタカナで入力してください"
+        required: "※ 名(カナ) は必須項目です",
+        katakana:"※ 名(カナ) は全角カタカナで入力してください"
       },
       "user[birth_year]":{
-        required: "\"年\" は必須項目です"
+        required: "※ \"年\" は必須項目です"
       },
       "user[birth_month]":{
-        required: "\"月\" は必須項目です"
+        required: "※ \"月\" は必須項目です"
       },
       "user[birth_day]":{
-        required: "\"日\" は必須項目です"
+        required: "※ \"日\" は必須項目です"
       },
       "user[phone_number]":{
-        required: "電話番号 は必須項目です",
-        telnum: "不正な電話番号が入力されました<br/><span>・0から始まる半角数字10~12桁</span><br/><span>・\"-\"(ハイフン)を抜いた連続した数字</span><br/>上記を確認し入力してください"
+        required: "※ 電話番号 は必須項目です",
+        telnum: "※ 不正な電話番号が入力されました<br/><span>・0から始まる半角数字10~12桁</span><br/><span>・\"-\"(ハイフン)を抜いた連続した数字</span><br/>上記を確認し入力してください"
       },
       "user[address_attributes][post_number]":{
-        required: "郵便番号 は必須項目です",
-        postnum: "不正な郵便番号が入力されました<br/><span>・連続した数字ではないか？</span><br/><span>・前半を半角数字3文字、後半を半角数字4文字</span><br/>上記を確認し入力してください"
+        required: "※ 郵便番号 は必須項目です",
+        postnum: "※ 不正な郵便番号が入力されました<br/><span>・連続した数字ではないか？</span><br/><span>・前半を半角数字3文字、後半を半角数字4文字</span><br/>上記を確認し入力してください"
       },
       "user[address_attributes][city]":{
-        required: "市区町村 は必須項目です"
+        required: "※ 市区町村 は必須項目です"
       },
       "user[address_attributes][address]":{
-        required: "番地 は必須項目です"
+        required: "※ 番地 は必須項目です"
       },
       "user[address_attributes][building_name]":{
-        maxlength: "建物名は25文字未満で入力してください"
+        maxlength: "※ 建物名は25文字未満で入力してください"
       },
       "user[address_attributes][building_tel]":{
-        telnum: "不正な電話番号が入力されました<br/><span>・0から始まる半角数字10~12桁</span><br/><span>・\"-\"(ハイフン)を抜いた連続した数字</span><br/>上記を確認し入力してください"
+        telnum: "※ 不正な電話番号が入力されました<br/><span>・0から始まる半角数字10~12桁</span><br/><span>・\"-\"(ハイフン)を抜いた連続した数字</span><br/>上記を確認し入力してください"
       }
     },
     errorPlacement: function(error, element) {
