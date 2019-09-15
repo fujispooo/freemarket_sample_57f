78c055bf8320a0f3ea1c2eaf5343186d3511b43f
fujipooo <majesty1579@yahoo.co.jp>
2019-09-15 15:07:30 +0900

    Update check_forbidden_word.js

--- app/assets/javascripts/check_forbidden_word.js
+++ app/assets/javascripts/check_forbidden_word.js
@@ -1,29 +1,31 @@
-function appendUser(user){
-  var html = `<p>
-                ※ <span>${user.forbidden_word}</span>は使用できません
-              </p>`;
-  $('#user-check-result').append(html);
-}
+window.addEventListener("turbolinks:load", function() {
+  function appendUser(user){
+    var html = `<p>
+                  ※ <span>${user.forbidden_word}</span>は使用できません
+                </p>`;
+    $('#user-check-result').append(html);
+  }
 
-$(function(){
-  $("#user_name").on("blur", function() {
-    $('#user-check-result').children().remove();
-    var nickname = $("#user_name").val();
-    $('.form__sent-btn').prop('disabled', false);
-    $.ajax({
-      type: 'GET',
-      url: '/jp/signup/check',
-      data: { user:{nickname: nickname} },
-      dataType: 'json'
-    })
-    .done(function(user){
-      $('#user-check-result').empty();
-      if (user.length !==0){
-        appendUser(user);
-        $('.form__sent-btn').prop('disabled', true);
-      }
-    })
-    .fail(function(){
+  $(function(){
+    $("#user_name").on("blur", function() {
+      $('#user-check-result').children().remove();
+      var nickname = $("#user_name").val();
+      $('.form__sent-btn').prop('disabled', false);
+      $.ajax({
+        type: 'GET',
+        url: '/jp/signup/check',
+        data: { user:{nickname: nickname} },
+        dataType: 'json'
+      })
+      .done(function(user){
+        $('#user-check-result').empty();
+        if (user.length !==0){
+          appendUser(user);
+          $('.form__sent-btn').prop('disabled', true);
+        }
+      })
+      .fail(function(){
+      });
     });
   });
 });
