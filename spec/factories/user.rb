7f9964eac4a5ced6daec4fd417436ae2dccc2e2a
fujipooo <majesty1579@yahoo.co.jp>
2019-09-19 10:05:23 +0900

    The transition process from the top page to the new member registration page has been proven by integration tests

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,17 +1,17 @@
 FactoryBot.define do
 
   factory :user do
-    nickname              {"abe"}
-    email                 {"kkk@gmail.com"}
-    password              {"00000000"}
-    password_confirmation {"00000000"}
+    nickname              {"永野芽郁"}
+    email                 {"kksfsfk@gmail.com.jp"}
+    password              {"mmmmmmmm"}
+    password_confirmation {"mmmmmmmm"}
     first_name            {"藤井"}
     last_name             {"涼"}
     first_name_kana       {"フジイ"}
     last_name_kana        {"リョウ"}
-    birth_year            {"1993"}
-    birth_month           {"12"}
-    birth_day             {"22"}
+    birth_year            {1993}
+    birth_month           {12}
+    birth_day             {22}
     phone_number          {"09039290303"}
   end
 end
